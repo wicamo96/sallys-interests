295dc1e2058e6709c1fff207ad658c38505dcb75
Will Moran <will.c.moran@gmail.com>
2024-04-25 10:41:26 -0400

    First commit

---
+++ c9/book-3/dog-walking/scripts/Walkers.js
@@ -0,0 +1,21 @@
+import { getWalkers } from "./database.js"
+
+const walkers = getWalkers()
+
+
+export const Walkers = () => {
+    let walkerHTML = "<ul>"
+
+    for (const walker of walkers) {
+        walkerHTML += `<li data-id="${walker.id}" 
+                            data-city="${walker.city}" 
+                            >${walker.name}
+                        </li>`
+    }
+
+    walkerHTML += "</ul>"
+
+    return walkerHTML;
+    
+}
+
